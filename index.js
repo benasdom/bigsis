d813945a629ccb0f9c26ea37e1b2a2618e28548a
Shadrack Darku <shaddydevops@gmail.com>
2025-03-16 14:42:45 +0000

    l

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let menupage=document.querySelector(".phonemenu");
+[...document.querySelectorAll(".svgmenu")][0]
+.onclick=()=>{
+    menupage.classList.toggle("slidein")
+
+
+
+
+}
+
+[...document.querySelectorAll(".svgmenu")][1]
+.onclick=()=>{
+menupage.classList.toggle("slidein")
+
+
+
+
+
+}
