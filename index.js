ce1ace4c03c5ee62940ac52011d5035df404b7f8
Shadrack Darku <shaddydevops@gmail.com>
2025-05-23 07:53:06 +0000

    major

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-
 let menupage=document.querySelector(".phonemenu");
 [...document.querySelectorAll(".svgmenu")][0]
 .onclick=()=>{
     menupage.classList.toggle("slidein")
-
-
-
-
 }
 
 [...document.querySelectorAll(".svgmenu")][1]
